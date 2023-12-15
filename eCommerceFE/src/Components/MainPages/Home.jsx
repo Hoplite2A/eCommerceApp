8cfc096c3a3bd11e10e6bcce5e490c9975326bb9
Hoplite2A <stgdave.mailloux#gmail.com>
2023-12-15 14:47:00 -0500

    Added Route & SignalState variable Template to Home, Coded out Home Page, Coded Header

---
+++ eCommerceFE/src/Components/MainPages/Home.jsx
@@ -0,0 +1,21 @@
+//! Imported Libraries -------------------------
+// NONE AT THIS TIME!
+//! --------------------------------------------
+
+//! Imported Components ------------------------
+import Header from "../UniversalFeatures/Navigation/Header";
+import SearchBar from "../UniversalFeatures/SearchFilters/SearchBar";
+import AllItems from "../MainPages/AllItems";
+import Footer from "../UniversalFeatures/Footer";
+//! --------------------------------------------
+
+export default function Home() {
+  return (
+    <div className="homeDiv">
+      <Header />
+      <SearchBar />
+      <AllItems />
+      <Footer />
+    </div>
+  );
+}
---
+++ eCommerceFE/src/Components/UniversalFeatures/Navigation/Header.jsx
@@ -0,0 +1,26 @@
+//! Imported Libraries -------------------------
+// import { Link } from 'react-router-dom';
+//! --------------------------------------------
+
+//! Imported Components ------------------------
+import Logo from "../Logo";
+import Navigation from "./Navigation";
+//! --------------------------------------------
+export default function Header() {
+  //TODO ---- Evaluation where we want to redirect endUser when
+  //TODO ---- clicking on siteName OR <Logo />
+  
+  return (
+    <div className="Header">
+      <div className="logo">
+        <Logo />
+      </div>
+      <div className="siteName">
+        <h1 className="siteNameText">eCommerce App</h1>
+      </div>
+      <div className="navBar">
+        <Navigation />
+      </div>
+    </div>
+  );
+}
