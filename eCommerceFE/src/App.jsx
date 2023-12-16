8ec95341641e01d4092e2e41342fb53f7eb5c239
Hoplite2A <stgdave.mailloux#gmail.com>
2023-12-16 17:28:39 -0500

    added Route paths to all linked items in Footer Component (they are commented out because they do not yet exist

--- eCommerceFE/src/App.jsx
+++ eCommerceFE/src/App.jsx
@@ -6,7 +6,8 @@ import { signal } from "@preact/signals-react";
 
 //! Imported Components ------------------------
 import Home from "./Components/MainPages/Home";
-import AccountDetails from './Components/LoggedInFeatures/AccountDetails';
+import AccountDetails from "./Components/LoggedInFeatures/AccountDetails";
+
 import "./App.css";
 //! --------------------------------------------
 
@@ -19,13 +20,13 @@ console.log(token.value);
 //*For Testing Purposes this is how to change a signal value:
 token.value = "89789yiuhUJHIYIUY899879hiuhkjhoiyiuHKJGHKjg987897yhjk";
 
-console.log(token.value); 
+console.log(token.value);
 export default function App() {
   //*Added State variable to App.jsx for propdrilling of JWT Token
   // const [token, setToken] = useState(null)
   //*Route syntax for useState Prop Drilling of JWT Token
   // <Route path='' element={} token={token} setToken={setToken}/>
-  
+
   //*Route template syntax utilizing SignalState for passing JWT Token to components:
   // <Route path='pathway' element={<Component />} />
   //*Import on each component where JWT is relevant as so (no need for props):
@@ -34,7 +35,20 @@ export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path='/AccountDetails' element={<AccountDetails />} />
+      {/* <Route path='/accountDetails' element={<AccountDetails />} /> */}
+      {/* <Route path='/about' element={<About />} /> */}
+      {/* <Route path='/careers' element={<Careers />} /> */}
+      {/* <Route path='/contactUs' element={<ContactUs />} /> */}
+      {/* <Route path='/csrStatement' element={<CSR />} /> */}
+      {/* <Route path='/greenInitiativeStatement' element={<Green />} /> */}
+      {/* <Route path='/membershipInfo' element={<MembershipInfo />} /> */}
+      {/* <Route path='/sellingInfo' element={<SellingInfo />} /> */}
+      {/* <Route path='/giftCards' element={<GiftCard />} /> */}
+      {/* <Route path='/reloadGiftCard' element={<Reload />} /> */}
+      {/* <Route path='/orderHistory' element={<OrderHistory />} /> */}
+      {/* <Route path='/returns' element={<Return />} /> */}
+      {/* <Route path='/warranty' element={<Warranty />} /> */}
+      {/* <Route path='/' element={< />} /> */}
     </Routes>
   );
 }
--- eCommerceFE/src/Components/UniversalFeatures/Footer.jsx
+++ eCommerceFE/src/Components/UniversalFeatures/Footer.jsx
@@ -9,56 +9,64 @@ export default function Footer() {
     
     return (
         <div className="footer">
-            <div className="getToKnowUs">
-                <h4 className="linksTitle">Get to Know Us</h4>
-                <Link to='/about'>
-                    <h5 className='footerLinks'>About</h5>
-                </Link>
-                <Link to='/careers'>
-                    <h5 className='footerLinks'>Careers</h5>
-                </Link>
-                <Link to='/contactUs'>
-                    <h5 className='footerLinks'>Contact Us</h5>
-                </Link>
-                <Link to='/csrStatement'>
-                    <h5 className='footgit push erLinks'>CSR Statement</h5>
-                </Link>
-                <Link to='/greenInitiativeStatement'>
-                    <h5 className='footerLinks'>Green Initiative Statement</h5>
-                </Link>
+            <div className="footerLinkSection">
+                <div className="getToKnowUs">
+                    <h4 className="linksTitle">Get to Know Us</h4>
+                    <Link to='/about'>
+                        <h5 className='footerLinks'>About</h5>
+                    </Link>
+                    <Link to='/careers'>
+                        <h5 className='footerLinks'>Careers</h5>
+                    </Link>
+                    <Link to='/contactUs'>
+                        <h5 className='footerLinks'>Contact Us</h5>
+                    </Link>
+                    <Link to='/csrStatement'>
+                        <h5 className='footgit push erLinks'>CSR Statement</h5>
+                    </Link>
+                    <Link to='/greenInitiativeStatement'>
+                        <h5 className='footerLinks'>Green Initiative Statement</h5>
+                    </Link>
+                </div>
+                <div className="membershipFeatures">
+                    <h4 className="linksTitle">Membership Features</h4>
+                    <Link to='/membershipInfo'>
+                        <h5 className="footerLinks">Membership Benefits</h5>
+                    </Link>
+                    <Link to='/sellingInfo'>
+                        <h5 className="footerLinks">How to Sell</h5>
+                    </Link>
+                </div>
+                <div className="giftCardInfo">
+                    <h4 className="giftCards">Gift Cards</h4>
+                    <Link to='/giftCards'>
+                        <h5 className="footerLinks">Purchase Gift Cards</h5>
+                    </Link>
+                    <Link to='/reloadGiftCard'>
+                        <h5 className="footerLinks">Reload Gift Card</h5>
+                    </Link>
+                </div>
+                <div className="manageAccount">
+                    <h4 className="linksTitle">Manage Account</h4>
+                    <Link to='/accountDetails'>
+                        <h5 className="footerLinks">Your Account</h5>
+                    </Link>
+                    <Link to='/orderHistory'>
+                        <h5 className="footerLinks">Your Orders</h5>
+                    </Link>
+                    <Link to='/returns'>
+                        <h5 className="footerLinks">Returns</h5>
+                    </Link>
+                    <Link to='/warranty'>
+                        <h5 className="footerLinks">Warranty Policy</h5>
+                    </Link>
+                </div>
             </div>
-            <div className="membershipFeatures">
-                <h4 className="linksTitle">Membership Features</h4>
-                <Link to='/membershipInfo'>
-                    <h5 className="footerLinks">Membership Benefits</h5>
-                </Link>
-                <Link to='/sellingInfo'>
-                    <h5 className="footerLinks">How to Sell</h5>
-                </Link>
-            </div>
-            <div className="giftCardInfo">
-                <h4 className="giftCards">Gift Cards</h4>
-                <Link to='/giftCards'>
-                    <h5 className="footerLinks">Purchase Gift Cards</h5>
-                </Link>
-                <Link to='/reloadGiftCard'>
-                    <h5 className="footerLinks">Reload Gift Card</h5>
-                </Link>
-            </div>
-            <div className="manageAccount">
-                <h4 className="linksTitle">Manage Account</h4>
-                <Link to='/accountDetails'>
-                    <h5 className="footerLinks">Your Account</h5>
-                </Link>
-                <Link to='/orderHistory'>
-                    <h5 className="footerLinks">Your Orders</h5>
-                </Link>
-                <Link to='/returnItems'>
-                    <h5 className="footerLinks">Returns</h5>
-                </Link>
-                <Link to='/warranty'>
-                    <h5 className="footerLinks">Warranty Policy</h5>
-                </Link>
+            <div className="socialMediaLinks">
+                <Link><img src="" alt="" /></Link>
+                <Link><img src="" alt="" /></Link>
+                <Link><img src="" alt="" /></Link>
+                <Link><img src="" alt="" /></Link>
             </div>
         </div>
     )
