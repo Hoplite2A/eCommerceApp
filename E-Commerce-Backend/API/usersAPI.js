3ae256ddb02a4c3017a5f94c61b874158c74c89c
dvirak <dvirak43@vt.edu>
2023-12-17 02:16:25 -0500

    Database running, initial users and products routes

--- E-Commerce-Backend/API/usersAPI.js
+++ E-Commerce-Backend/API/usersAPI.js
@@ -1,14 +1,23 @@
+require("dotenv").config();
 const express = require("express");
 const usersRouter = express.Router();
-
-const { createUser, getUserByUsername, getAllUsers } = require("../db/users");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = process.env;
+const { requireUser } = require("./utils");
+const {
+  createUser,
+  getUserByUsername,
+  getAllUsers,
+  getUser,
+  getUserById,
+  updateUser,
+} = require("../db/users");
 
 // Get /api/users
-usersRouter.get("/users", async (req, res, next) => {
-  console.log("We here in get users API");
+usersRouter.get("/", async (req, res, next) => {
   try {
     const users = await getAllUsers();
-    res.send(users);
+    res.send({ users });
   } catch (error) {
     next(error);
   }
@@ -17,7 +26,20 @@ usersRouter.get("/users", async (req, res, next) => {
 // POST /api/users/register
 usersRouter.post("/register", async (req, res, next) => {
   try {
-    const { username, password } = req.body;
+    const {
+      username,
+      password,
+      fName,
+      lName,
+      pName,
+      streetAddress,
+      apt,
+      city,
+      state,
+      zip,
+      phone,
+      email,
+    } = req.body;
     const queriedUser = await getUserByUsername(username);
     if (queriedUser) {
       res.status(401);
@@ -35,6 +57,16 @@ usersRouter.post("/register", async (req, res, next) => {
       const user = await createUser({
         username,
         password,
+        fName,
+        lName,
+        pName,
+        streetAddress,
+        apt,
+        city,
+        state,
+        zip,
+        phone,
+        email,
       });
       if (!user) {
         next({
@@ -53,6 +85,87 @@ usersRouter.post("/register", async (req, res, next) => {
   } catch (error) {
     next(error);
   }
+});
+
+// POST /api/users/login
+usersRouter.post("/login", async (req, res, next) => {
+  const { username, password } = req.body;
+
+  // request must have both
+  if (!username || !password) {
+    next({
+      name: "MissingCredentialsError",
+      message: "Please supply both a username and password",
+    });
+  }
+
+  try {
+    const user = await getUser({ username, password });
+    if (!user) {
+      next({
+        name: "IncorrectCredentialsError",
+        message: "Username or password is incorrect",
+      });
+    } else {
+      const token = jwt.sign(
+        { id: user.id, username: user.username },
+        JWT_SECRET,
+        { expiresIn: "1w" }
+      );
+      res.send({ user, message: "you're logged in!", token });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+usersRouter.patch("/:userId", async (req, res, next) => {
+  const { userId } = req.params;
+  console.log("userId");
+  console.log(userId);
+  const { username, password, name } = req.body;
+  console.log("req.body");
+  console.log([req.body]);
+  const body = Object.entries(req.body);
+
+  const updateFields = {};
+
+  body.forEach((element) => {
+    console.log(typeof element[0]);
+    const fieldName = element[0];
+    console.log(fieldName);
+    updateFields.fieldName = element[1];
+    console.log(updateFields);
+  });
+
+  // if (name) {
+  //   updateFields.name = name;
+  // }
+
+  // if (password) {
+  //   updateFields.name = name;
+  // }
+
+  // if (username) {
+  //   updateFields.name = name;
+  // }
+
+  // try {
+  //   const originalUser = await getUserById(userId);
+  //   if (originalUser.user.id === req.user.id) {
+  //     const updatedUser = await updateUser(userId, updateFields);
+  //     res.send({ user: updatedUser });
+  //   } else {
+  //     next({
+  //       name: "UnauthorizedUserError",
+  //       message: "You cannot update another user.",
+  //     });
+  //   }
+  // } catch ({ name, message }) {
+  //   next({ name, message });
+  // }
+  res.send(updateFields);
+  // res.send("PATCH EDIT USER END");
 });
 
 module.exports = usersRouter;
--- E-Commerce-Backend/db/users.js
+++ E-Commerce-Backend/db/users.js
@@ -2,7 +2,20 @@ const client = require("./client");
 const bcrypt = require("bcrypt");
 const SALT_COUNT = 10;
 
-async function createUser({ username, password, name }) {
+async function createUser({
+  username,
+  password,
+  fName,
+  lName,
+  pName,
+  streetAddress,
+  apt,
+  city,
+  state,
+  zip,
+  phone,
+  email,
+}) {
   // use bcrypt to "hash" a function by SALT_COUNT to create unique hashedPassword that is more secure
   const hashedPassword = await bcrypt.hash(password, SALT_COUNT);
   try {
@@ -10,11 +23,24 @@ async function createUser({ username, password, name }) {
       rows: [user],
     } = await client.query(
       `
-      INSERT INTO users(username, password, name) VALUES ($1, $2, $3)
+      INSERT INTO users(username, password, first_name, last_name, pname, address, apartment, city, state, zip, phone, email) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)
       ON CONFLICT (username) DO NOTHING 
-      RETURNING id, username
+      RETURNING *
     `,
-      [username, hashedPassword, name]
+      [
+        username,
+        hashedPassword,
+        fName,
+        lName,
+        pName,
+        streetAddress,
+        apt,
+        city,
+        state,
+        zip,
+        phone,
+        email,
+      ]
     );
     return user;
   } catch (error) {
@@ -28,7 +54,7 @@ async function getUserByUsername(userName) {
     // Try to get username directly first, if doesnt work go to the delete password method
     const { rows } = await client.query(
       `
-    SELECT username
+    SELECT username, password, id
     FROM users
     WHERE username = $1;
     `,
@@ -38,6 +64,7 @@ async function getUserByUsername(userName) {
     if (!rows || !rows.length) return null;
     // if it exists, delete password from return object
     // delete user.password
+    const [user] = rows;
     return user;
   } catch (error) {
     console.log(error);
@@ -52,6 +79,76 @@ async function getAllUsers() {
     `);
     return rows;
   } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+async function getUser({ username, password }) {
+  if (!username || !password) {
+    return;
+  }
+  try {
+    const user = await getUserByUsername(username);
+    if (!user) return;
+    console.log(user);
+    const hashedPassword = user.password;
+    const passwordsMatch = await bcrypt.compare(password, hashedPassword);
+    if (!passwordsMatch) return;
+    delete user.password;
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getUserById(userId) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `
+      SELECT *
+      FROM users
+      WHERE id=$1;
+      `,
+      [userId]
+    );
+    if (!user)
+      throw {
+        name: "UserNotFoundError",
+        message: "A username with that id does not exist",
+      };
+    delete user.password;
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function updateUser(id, fields = {}) {
+  const setString = Object.keys(fields)
+    .map((key, index) => `"${key}" = $${index + 1}`)
+    .join(",");
+
+  if (setString.length === 0) {
+    return;
+  }
+
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `
+        UPDATE users
+        SET ${setString}
+        WHERE id=${id}
+        RETURNING *;
+        `,
+      Object.values(fields)
+    );
+    return user;
+  } catch (error) {
     throw error;
   }
 }
@@ -60,4 +157,7 @@ module.exports = {
   createUser,
   getAllUsers,
   getUserByUsername,
+  getUser,
+  getUserById,
+  updateUser,
 };
