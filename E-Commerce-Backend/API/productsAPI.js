c413c3822f6abc57f41f8d1c587ff3d2373d2360
Hoplite2A <stgdave.mailloux#gmail.com>
2023-12-21 00:01:29 -0500

    added requireUser to product API components for data validation.

--- E-Commerce-Backend/API/productsAPI.js
+++ E-Commerce-Backend/API/productsAPI.js
@@ -10,7 +10,8 @@ const {
   getSingleProduct,
   createProduct,
   verifyProduct,
-  updateProduct,
+    updateProduct,
+    deleteProduct
 } = require("../db/products");
 //! ---------------------------------------------
 
@@ -23,7 +24,10 @@ productsRouter.get("/", async (req, res, next) => {
     console.log(
       `An Error ocurred within the poductsRouter.get('/') API Route, ${{title, message}}`
     );
-    next({title, message});
+    next({
+        error: err,
+        message: `Failed to fetch all items. Please reattempt or contact support via the Contact Us page.`
+    });
   }
 });
 //* -----------------GET ALL API-----------------
@@ -36,21 +40,24 @@ productsRouter.get('/:id', async (req, res, next) => {
         res.send({ singleProduct });
     } catch (err) {
         console.log(`An Error ocurred in productsRouter.get('/:id') API Route, ${err}`);
-        next(err);
+        next({
+            error: err,
+            message: `Failed to fetch single item details. Please reattempt or contact support via the Contact Us page.`
+        });
     }
 });
 //* ---------------GET SINGLE API----------------
 
 //* -------------CREATE PRODUCT API--------------
-productsRouter.post('/', async (req, res, next) => {
-    const { title, price, description, category, image } = req.body;
+productsRouter.post('/', requireUser, async (req, res, next) => {
+    const { title, price, description, category, image , sellerId} = req.body;
     try {
 
-        const newProduct = await verifyProduct(title, description, category);
+        const newProduct = await verifyProduct(title, description, category, sellerId);
         
         if (newProduct) {
             next({
-              name: {title},
+              name: 'DuplicateItemDetected',
               message: `${newItem.title} already exists. Please update the existing product. Find item by ID: ${newItem.id}`
             })
         }
@@ -60,7 +67,8 @@ productsRouter.post('/', async (req, res, next) => {
             price, 
             description, 
             category, 
-            image
+            image,
+            sellerId
         })
 
         res.send({
@@ -68,18 +76,26 @@ productsRouter.post('/', async (req, res, next) => {
         })
     } catch (err) {
         console.log(`An Error ocurred in the productsRouter.post('/'), ${err}`);
-        next(err);
+        next({
+            error: err,
+            message: `Failed to create new item. Please reattempt or contact support via the Contact Us page.`
+        });
     }
 });
 //* -------------CREATE PRODUCT API-------------- 
 
 //* -------------UPDATE PRODUCT API-------------- 
-productsRouter.patch('/:id', async (req, res, next) => {
+productsRouter.patch('/:id', requireUser, async (req, res, next) => {
+//! -------- To update the product the req will have to include the following: 
     const { productId } = req.params;
-    const { title, price, description, category, image } = req.body;
-
+    const { userId, title, price, description, category, image } = req.body;
+    //TODO ---- userId will have to be included in the req.body
+//! -------------------------------------------------------------------------- 
+    
     const updateFields = {};
 
+    //Combining the different req.body values based on if there is a value > 0
+    //and redefining it for consumption as a new array
     if (title && title.length > 0) {
         updateFields.title = title;
     }
@@ -99,13 +115,13 @@ productsRouter.patch('/:id', async (req, res, next) => {
     try {
         const originalProductDetails = await getSingleProduct(id);
 
-        if (originalProductDetails.owner.id === req.user.id) {
-            const updateProduct = await updateProduct(productId, updateFields)
-            res.send({ product: updateProduct })
+        if (originalProductDetails.owner.id === userId) {
+            const updatedProduct = await updateProduct(productId, updateFields)
+            res.send({ product: updatedProduct })
         } else {
             console.log(`Unauthorized Attempt to edit item ${productId}`)
             next({
-                name: UnauthorizedItemEdit,
+                name: 'UnauthorizedItemEdit',
                 message: `This attempt to edit this item (${productId}) is Unauthorized, please log in to edit this item.`
             });
         }
@@ -116,6 +132,34 @@ productsRouter.patch('/:id', async (req, res, next) => {
             error: err,
             message: `${title} failed to update. Please reattempt or contact support via the Contact Us page.`
         });
-  }
+    }
 })
-//* -------------UPDATE PRODUCT API-------------- 
+//* -------------UPDATE PRODUCT API--------------
+
+//* -------------DELETE PRODUCT API--------------
+productsRouter.delete('/:id', requireUser, async (req, res, next) => {
+    //TODO ---- requireUser in async params???????????
+    const { productId } = req.params;
+    const { userId, sellerId } = req.body; 
+    
+    try {
+        //Validation that the user deleting this item is the original seller
+        if (userId === sellerId) {
+            const deletedProduct = await deleteProduct(productId)
+        } else {
+            next({
+                name: 'NotAuthorizedSeller',
+                message: 'Must be the original seller or SysAdmin to delete this item.'
+            })
+        }
+    } catch (err) {
+        console.log(`An Error ocurred in productsRouter.delete('/:id') (API), ${err}`);
+        next({
+            error: err,
+            message: `${title} failed to delete. Please reattempt or contact support via the Contact Us page.`
+        });
+    }
+})
+
+
+//* -------------DELETE PRODUCT API--------------
