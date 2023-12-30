b422c13e80ef006716469637fe10894d3c44c08f
Hoplite2A <stgdave.mailloux#gmail.com>
2023-12-29 20:11:00 -0500

    prep work for next styling branch

--- eCommerceFE/src/Components/MainPages/ReturnPolicy.jsx
+++ eCommerceFE/src/Components/MainPages/ReturnPolicy.jsx
@@ -24,7 +24,7 @@ export default function ReturnPolicy() {
 
           <h3 className="sectionHeaders">Eligibility</h3>
           <br></br>
-          <p className='body'>Items eligible for return must be unused, in their original
+          <p className='bodyContent'>Items eligible for return must be unused, in their original
           packaging, and accompanied by a valid proof of purchase. Certain
           items, such as perishables, personalized products, and downloadable
           software, are non-returnable.</p>
@@ -33,14 +33,14 @@ export default function ReturnPolicy() {
 
           <h3 className="sectionHeaders">Return Window</h3>
           <br></br>
-          <p className='body'>Customers have {numOfDays} days from the date of delivery to
+          <p className='bodyContent'>Customers have {numOfDays} days from the date of delivery to
           initiate a return. After this period, returns may not be accepted.</p>
         </div>
         <div className="articleDiv">
 
           <h3 className="sectionHeaders">Return Process</h3>
           <br></br>
-          <p className='body'>To initiate a return, customers must contact our customer service
+          <p className='bodyContent'>To initiate a return, customers must contact our customer service
           team through the designated channels provided on our website. A
           return authorization (RA) number will be issued upon approval.</p>
         </div>
@@ -48,7 +48,7 @@ export default function ReturnPolicy() {
                 
           <h3 className="sectionHeaders">Condition of Returned Items</h3>
           <br></br>
-          <p className='body'>Returned items will be inspected upon receipt. Items must be in new,
+          <p className='bodyContent'>Returned items will be inspected upon receipt. Items must be in new,
           unused condition to be eligible for a refund. Any damage or signs of
           use may result in a partial refund or denial of the return.</p>
         </div>
@@ -56,7 +56,7 @@ export default function ReturnPolicy() {
 
           <h3 className="sectionHeaders">Refund Method</h3>
           <br></br>
-          <p className='body'>Refunds will be processed using the same method as the original
+          <p className='bodyContent'>Refunds will be processed using the same method as the original
           payment. If the original payment method is unavailable, store credit
           will be issued.</p>
         </div>
@@ -64,7 +64,7 @@ export default function ReturnPolicy() {
 
           <h3 className="sectionHeaders">Restocking Fee</h3>
           <br></br>
-          <p className='body'>A restocking fee may apply to certain returns, especially
+          <p className='bodyContent'>A restocking fee may apply to certain returns, especially
           for large or specialized items. This fee will be clearly
           communicated during the return authorization process.</p>
         </div>
@@ -72,7 +72,7 @@ export default function ReturnPolicy() {
 
           <h3 className="sectionHeaders">Fraud Prevention</h3>
           <br></br>
-          <p className='body'>To protect against fraudulent returns, we reserve the right to deny
+          <p className='bodyContent'>To protect against fraudulent returns, we reserve the right to deny
           returns that exhibit suspicious patterns or abuse of our return
           policy.</p>
         </div>
@@ -80,7 +80,7 @@ export default function ReturnPolicy() {
 
           <h3 className="sectionHeaders">Exclusions</h3>
           <br></br>
-          <p className='body'>Certain items, as specified in the product description, are
+          <p className='bodyContent'>Certain items, as specified in the product description, are
           non-returnable. Additionally, final sale items are not eligible for
           return.</p>
         </div>
@@ -88,7 +88,7 @@ export default function ReturnPolicy() {
 
           <h3 className="sectionHeaders">Return Shipping</h3>
           <br></br>
-          <p className='body'>Customers are responsible for return shipping costs unless the
+          <p className='bodyContent'>Customers are responsible for return shipping costs unless the
           return is due to an error on our part. We recommend using a
           trackable shipping method for returns.</p>
         </div>
@@ -96,21 +96,21 @@ export default function ReturnPolicy() {
 
           <h3 className="sectionHeaders">International Returns</h3>
           <br></br>
-          <p className='body'>International returns may be subject to additional shipping fees and
+          <p className='bodyContent'>International returns may be subject to additional shipping fees and
           customs duties. Customers are responsible for these costs.</p>
         </div>
         <div className="articleDiv">
 
           <h3 className="sectionHeaders">Modification of the Policy</h3>
           <br></br>
-          <p className='body'>We reserve the right to modify this return policy at any time.
+          <p className='bodyContent'>We reserve the right to modify this return policy at any time.
           Customers will be notified of any changes through our official
           communication channels.</p>
         </div>
         <div className="articleDiv">
           <h3 className="sectionHeaders">Commitment to Customer Satisfaction</h3>
           <br></br>
-          <p className='body'>Our goal is to provide a fair and transparent return process. If you
+          <p className='bodyContent'>Our goal is to provide a fair and transparent return process. If you
           have any concerns or questio      ns regarding our return policy, please
           contact our customer service team. We are here to assist you.</p>
         </div>
