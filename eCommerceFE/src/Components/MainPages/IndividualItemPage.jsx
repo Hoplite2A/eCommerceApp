ce559cd795f691f05ba1f006e85d2cbc3b31ce0d
Hoplite2A <stgdave.mailloux#gmail.com>
2024-01-06 15:29:17 -0500

    styling and navigation optimization for allitems and individualitems and inditem tiles.

--- eCommerceFE/src/Components/MainPages/IndividualItemPage.jsx
+++ eCommerceFE/src/Components/MainPages/IndividualItemPage.jsx
@@ -1,75 +1,88 @@
 //! Imported Libraries -------------------------
 import { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-import {Link} from 'react-router-dom';
+import { Link, useParams, useNavigate } from 'react-router-dom';
 //! --------------------------------------------
 
 //! Imported Components/Variables---------------
+import Header from '../UniversalFeatures/Navigation/Header';
+import Footer from '../UniversalFeatures/Footer';
 import { token } from "../UniversalFeatures/Login";
 import { BASE_URL } from '../../App';
 //! --------------------------------------------
 
 
 export default function IndividualItemPage() {
+
+    const navigate = useNavigate();
+    const allItemsPage = () => {
+        navigate('/AllItems');
+    }
+
+    //! To Test logged in versus visitor view enable 
+    //! line titled Not-Visible and comment out Visible
+    //*Not-Visible:
+    // const visible = !token.value;
+    //*Visible: 
+    const visible = !token.value;
     
-    // const [item, setItem] = useState({});
-
-    // const { id } = useParams();
-
-    // useEffect(() => {
-    //     async function IndividualItemPageFetch() {
-    //         try {
-    //             const res = await fetch(`${BASE_URL}/products/${id}`);
-    //             const json = await res.json();
-    //             console.log(json);
-    //             setItem(json.singleProduct);
-    //         } catch (err) {
-    //             console.log(`Error occurred in the IndividualItemPageFetch within the IndividualItemPage component, ${err}`);
-    //             return (<>
-    //                 <div className="allItemsErrorMessage">
-    //                     <p className="errorMessage">
-    //                         Aparently our servers needed a coffee break. Once they are re-cafinated, they will provide you the info you requested.
-    //                     </p>
-    //                 </div>
-    //             </>)
-    //         }
-    //     }
-    //     return () => IndividualItemPageFetch();
-    // }, [])
-
-    //TODO ----- Testing Optimized Prop Drilling Via useLocation
-    // const location = useLocation();
-    // const {fromIndividualItemPage} = location.state;
-    // let item = fromIndividualItemPage.data;
-    // console.log(item);
-    //TODO -----------------------------------------------------
-
-    //! TESTING
-    const location = useLocation();
-    const myObject = location.state.indItem;
-    const item = myObject;
-
+    //Pulled param for use in useEffect API Call for individual Item Details
+    const { id } = useParams();
+    
+    //Defined Object to destructure and use for function return fields from 
+    //the useEffect API Call
+    const [item, setItem] = useState({});
+    
+    useEffect(() => {
+    async function IndividualItemPageFetch() {
+        try {
+            const res = await fetch(`${BASE_URL}/products/${id}`);
+            const json = await res.json();
+            console.log(json);
+            setItem(json.singleProduct);
+        } catch (err) {
+                console.log(`Error occurred in the IndividualItemPageFetch within the IndividualItemPage component, ${err}`);
+                return (<>
+                    <div className="allItemsErrorMessage">
+                        <p className="errorMessage">
+                            Aparently our servers needed a coffee break. Once they are re-cafinated, they will provide you the info you requested.
+                        </p>
+                    </div>
+                </>)
+            }
+        }
+        return () => IndividualItemPageFetch();
+    }, [id])
+    
     return (<>
-       <div className="individualItemPage">
-            <div className="individualItemPageTop">
+        <Header />
+        <div className="individualItemPage">
+            <div className="individualItemPageLeft">
                     <div className="individualItemPageImage">
                         <img src={item.image} alt={item.title} />
                     </div>
             </div>
-            <div className="individualItemPageBottom">
-                <h3 className="individualTitle">{item.title}</h3>
-                <p className="individualItemPagePrice price">{item.price}</p>
-            </div>
-            <div className="individualItemPageButton">
-            {token.value ? <>
-                        <button className="wishlistButton individualItemPageButton">Add to Wishlist</button>
-                        <button className="cartButton individualItemPageButton">Add to Cart</button>
-                    </> :
-                    <>
-                        <button className="cartButton individualItemPageButton">Add to Cart</button>
-                    </>}
+            <div className="individualItemPageRight">
+                    <h3 className="individualItemPageTitle">{item.title}</h3>
+                <div className="individualItemPageInfo">
+                    <div className="individualItemPagePriceDiv">
+                        <p className="individualItemPagePrice price">${item.price}</p>
+                    </div>
+                    <div className={visible ? "individualItemPageButtonLoggedIn" : "individualItemPageButtonVisiting"}>
+                    {/* <div className="individualItemPageButtonVisiting individualItemPageButtonLoggedIn"> */}
+                    {visible ? <>
+                                <button className="wishlistButton individualItemPageButton">Add to Wishlist</button>
+                                <button className="cartButton individualItemPageButton">Add to Cart</button>
+                            </> :
+                            <>
+                                <button className="cartButton individualItemPageButton">Add to Cart</button>
+                            </>}
+                    </div>  
+                </div>
             </div>
         </div>
+            <p className="backMessage">Not digging this, check out other items</p>
+            <button className="backToHome" onClick={allItemsPage}>OTHER ITEMS</button>
+        <Footer />
     </>)
 }
 
